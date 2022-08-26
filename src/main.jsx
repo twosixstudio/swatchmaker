94ea081db9387ae439f32116911e07d683f50b94
Tom Penfold <tom@twosix.studio>
2022-08-26 19:20:52 +0100

    added ga

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactGA from "react-ga4";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+ReactGA.initialize([
+  {
+    trackingId: "G-P5RJB2PCNW",
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
